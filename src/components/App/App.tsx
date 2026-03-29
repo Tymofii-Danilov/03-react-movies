33d0f07af5b7db9d6757dbc2e492974f6115ea9d
Tymofii Danilov <timadanilov2002@gmail.com>
2026-03-29 17:08:41 +0200

    fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import "./App.module.css";
-import fetchMovies from "../services/movieService";
+import fetchMovies from "../../services/movieService";
 import toast, { Toaster } from "react-hot-toast";
-import type { Movie } from "../types/movie";
+import type { Movie } from "../../types/movie";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieGrid from "../MovieGrid/MovieGrid";
